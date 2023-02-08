2da07720b107efb80bb65a3d761ef2146ccee634
Nitin Karmuse <nitin.karmuse@Nitins-MacBook-Air.local>
2023-02-08 20:18:20 +0000

    create two functions.

--- milestone_2.py
+++ milestone_2.py
@@ -1,19 +1,37 @@
 import random
 
-word_list = ["Mango","Banana","Apple","Orange","Grapes"]
+def ask_for_input():
+    while True:
+        guess = input("Enter a letter: ")
+        if len(guess) == 1 and guess.isalpha():
+            print("Good Guess.")
+            break
+        else:
+            print("Oops! That is not a valid input.")
+    check_guess(guess)
+    
+def check_guess(guess): 
+    g = guess.lower()
+    if g in word:
+        print(f"Good guess {guess} is in the word. ")
+    else:
+        print(f"Sorry {guess} is not in the word. Try again.")
+
+
+word_list = ["mango","banana","apple","orange","grapes"]
 print(word_list)
 word = random.choice(word_list)
 print(word)
-while True:
+'''while True:
     guess = input("Enter a letter: ")
     if len(guess) == 1 and guess.isalpha():
         print("Good Guess.")
         break
     else:
-        print("Oops! That is not a valid input.")
+        print("Oops! That is not a valid input.")'''
 
-if guess in word:
-    print(f"Good guess {guess} is in the word. ")
-else:
-    print(f"Sorry {guess} is not in the word. Try again.")
-    
+ask_for_input()
+
+
+
+
--- milestone_3.py
+++ milestone_3.py
@@ -1,8 +1,18 @@
-while True:
-    guess = input("Enter a letter: ")
-    if len(guess) == 1 and guess.isalpha():
-        print("Good Guess.")
-        break
-    else:
-        print("Oops! That is not a valid input.")
+def ask_for_input():
+    while True:
+        guess = input("Enter a letter: ")
+        if len(guess) == 1 and guess.isalpha():
+            print("Good Guess.")
+            break
+        else:
+            print("Oops! That is not a valid input.")
+    check_guess(guess)
 
+def check_guess(guess): 
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess {guess} is in the word. ")
+    else:
+        print(f"Sorry {guess} is not in the word. Try again.")
+
+ask_for_input()
