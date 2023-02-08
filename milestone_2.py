6b0187ac8db02fe7eca8818b6251a349633078e6
Nitin Karmuse <nitin.karmuse@Nitins-MacBook-Air.local>
2023-02-08 19:16:33 +0000

    adds input function

--- milestone_2.py
+++ milestone_2.py
@@ -5,3 +5,7 @@ print(word_list)
 word = random.choice(word_list)
 print(word)
 guess = input("Enter a letter: ")
+if len(guess) == 1 and guess.isalpha():
+    print("Good Guess.")
+else:
+    print("Oops! That is not a valid input.")
