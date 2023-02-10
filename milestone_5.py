fd7ecc707bd10096cdd89566240d668b8ab19fb1
Nitin Karmuse <nitin.karmuse@Nitins-MacBook-Air.local>
2023-02-10 15:31:10 +0000

    attribute word.guessed changed

--- milestone_5.py
+++ milestone_5.py
@@ -4,11 +4,11 @@ class Hangman:
         self.word_list = word_list
         self.word = random.choice(self.word_list)
         self.num_lives = num_lives
-        self.word_guesses = []
+        self.word_guessed = []
         self.num_letters = len(self.word)
         self.list_of_guesses = []
         for i in self.word:
-            self.word_guesses.append("_")
+            self.word_guessed.append("_")
 
     def check_guess(self, guess): 
         #print("word is - ", self.word)
@@ -19,15 +19,15 @@ class Hangman:
             idx = 0
             for i in self.word:
                 if self.guess == i:
-                    self.word_guesses[idx] = self.guess 
-                    print(self.word_guesses)
+                    self.word_guessed[idx] = self.guess 
+                    print(self.word_guessed)
                     self.num_letters = self.num_letters - 1  
                 idx = idx + 1              
         else:
             self.num_lives = self.num_lives -1  
             print(f"Sorry {self.guess} is not in the word. Try again.")
             print(f"you have {self.num_lives} lives left.")
-            print(self.word_guesses)
+            print(self.word_guessed)
             
          
         
