1b622572c25f125aa501789169075ac7eefe2a90
Nitin Karmuse <nitin.karmuse@Nitins-MacBook-Air.local>
2023-02-11 17:16:01 +0000

    chenages in num_letter

--- milestone_4.py
+++ milestone_4.py
@@ -6,10 +6,19 @@ class Hangman:
         self.word = random.choice(self.word_list)
         self.num_lives = num_lives
         self.word_guessed = []
-        self.num_letters = 0
+        
         self.list_of_guesses = []
         for i in self.word:
             self.word_guessed.append("_")
+        temp = set()
+        temp_length = 0
+        for i in self.word:
+            temp.add(i)
+            temp_length = len(temp)
+        print("temp_length: ", temp_length)
+
+        self.num_letters = temp_length
+                
 
     def check_guess(self, guess): 
         
@@ -18,20 +27,18 @@ class Hangman:
         self.guess = self.guess.lower()
         if self.guess in self.word:
             print(f"Good guess {self.guess} is in the word. ")
+            idx = 0
             for i in self.word:
                 if self.guess == i:
-                    self.word_guessed[self.num_letters] = self.guess 
+                    self.word_guessed[idx] = self.guess 
                     print(self.word_guessed)
-                    #self.num_letters = self.num_letters + 1  
-                    #self.num_lives = self.num_lives -1       
+                idx = idx + 1     
         else:
             self.num_lives = self.num_lives -1  
             print(f"Sorry {self.guess} is not in the word. Try again.")
             print(f"you have {self.num_lives} lives left.")
-
-        self.word_guessed[self.num_letters] = self.guess 
-        print(self.word_guessed)
-        self.num_letters = self.num_letters + 1  
+            print(self.word_guessed)
+        
     
     def ask_for_input(self):
 
