fa0ed871b6fb3c1cd36f3e1d678e382b50308592
Nitin Karmuse <nitin.karmuse@Nitins-MacBook-Air.local>
2023-02-12 12:48:02 +0000

    changes in ask_for_input

--- milestone_4.py
+++ milestone_4.py
@@ -22,37 +22,35 @@ class Hangman:
 
     def check_guess(self, guess): 
         
-        print("word is - ", self.word)
         self.guess = guess
         self.guess = self.guess.lower()
+       
         if self.guess in self.word:
-            print(f"Good guess {self.guess} is in the word. ")
+            print(f"Good guess! {self.guess} is in the word.")
             idx = 0
             for i in self.word:
                 if self.guess == i:
-                    self.word_guessed[idx] = self.guess 
+                    self.word_guessed[idx] = self.guess
+                    
                     print(self.word_guessed)
                 idx = idx + 1  
             self.num_letters = self.num_letters - 1
         else:
             self.num_lives = self.num_lives -1  
-            print(f"Sorry {self.guess} is not in the word.")
-            print(f"you have {self.num_lives} lives left.")
+            print(f"Sorry, {self.guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
             print(self.word_guessed)
-        self.list_of_guesses.append(self.guess)
+        
     
     def ask_for_input(self):
-
+        while True:
             self.guess = input("Enter a letter: ")
-            if len(self.guess) == 1 and self.guess.isalpha():
-                if self.guess in self.list_of_guesses:
-                    print("You have already tried that letter.")
-                else:
-                    print("Good Guess.")
-                    self.check_guess(self.guess)
-                #break
+            if len(self.guess) != 1:
+                    print("Invalid letter. Please, enter a single characher.")
+            elif self.guess in self.list_of_guesses:
+                    print("You already tried the letter!")
             else:
-                print("Invalid input.")
+                self.check_guess(self.guess)
     
 hangman = Hangman(["mango","orange","apple","peach","grape"], 5 )
 hangman.ask_for_input()
